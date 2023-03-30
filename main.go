9b5612034dde9e494376b0670c8919b8a6f4c94a
marocchino <marocchino@users.noreply.github.com>
2023-03-30 09:21:52 +0900

    🐛 Conditional statement missing `return`
    This change adds a `return` statement at the end of the conditional statement,
    to make sure that the program does not continue after it.

--- main.go
+++ main.go
@@ -35,6 +35,7 @@ func main() {
 	}
 	if output == "" {
 		fmt.Println("No staged changes.")
+		return
 	}
 	p := fmt.Sprintf("You are to act as the author of a commit message in git. Your mission is to create clean and comprehensive commit messages in the gitmoji convention with emoji and explain why a change was done. I'll send you an output of 'git diff --staged' command, and you convert it into a commit message. Add a short description of WHY the changes are done after the commit message. Don't start it with 'This commit', just describe the changes. Use the present tense. Commit title must not be longer than 74 characters.\n%s", output)
 	result, err := generateText(p, *maxTokens)
